74d417115bc91f20ffd086fcaa69976b3cbc673c
Fitrah Arie Ramadhan <56785130+Fitrah1812@users.noreply.github.com>
2021-03-30 07:49:58 +0700

    Update Termometer.c

--- Source_code/Termometer.c
+++ Source_code/Termometer.c
@@ -9,40 +9,38 @@ int fahrenheit = 0;
 
 void setup()
 {
-  pinMode(A0, INPUT);
-  //pinMode(tmp,INPUT);
-  Serial.begin(9600);
-
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+    pinMode(A0, INPUT);
+    Serial.begin(9600);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
 }
 
 void loop()
 {
-  baselineTemp = 40;
-  
-  celsius = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
-  
-  fahrenheit = ((celsius * 9) / 5 + 32);
-  Serial.print(celsius);
-  Serial.print(" C, ");
-  Serial.print(fahrenheit);
-  Serial.println(" F");
-  value = analogRead(tmp)*0.004882814;
-  value = (value - 0.5) * 100.0;
-  
-  if (celsius < baselineTemp && celsius > 27) {
-    digitalWrite(7, LOW);
-    digitalWrite(6, HIGH);
-  }
-  else if (celsius >= baselineTemp) {
-    digitalWrite(7, HIGH);
-    digitalWrite(6, LOW);
-  }
-  lcd.setCursor(0,1);
-  lcd.print("Tmp:");
-  lcd.print(value);
-  delay(1000);
-  lcd.clear();
+    baselineTemp = 40;
+
+    celsius = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
+
+    fahrenheit = ((celsius * 9) / 5 + 32);
+    Serial.print(celsius);
+    Serial.print(" C, ");
+    Serial.print(fahrenheit);
+    Serial.println(" F");
+    value = analogRead(tmp)*0.004882814;
+    value = (value - 0.5) * 100.0;
+
+    if (celsius < baselineTemp && celsius > 27) {
+      digitalWrite(7, LOW);
+      digitalWrite(6, HIGH);
+    }
+    else if (celsius >= baselineTemp) {
+      digitalWrite(7, HIGH);
+      digitalWrite(6, LOW);
+    }
+    lcd.setCursor(0,1);
+    lcd.print("Tmp:");
+    lcd.print(value);
+    delay(1000);
+    lcd.clear();
 }
